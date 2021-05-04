cd0a2d4ed553b396152ee82c44eebcf4c924887e
jessica you <jessicaxytan@gmail.com>
2021-05-03 21:23:58 -0400

    commit tree 84f2b63d61edb1e956f8ad74213dda8f1c57bea7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    const a = drivers[0];
+    const b = drivers[1];
+    return[a,b];
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.splice(-2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
